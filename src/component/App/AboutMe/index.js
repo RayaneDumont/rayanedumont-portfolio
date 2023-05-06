37f2dcaacf169b86e6e0a94de12f4fed98eda9f0
RayaneDumont <rayane.dumont@gmail.com>
2023-05-06 19:14:28 +0200

    adding styles to the about me page + adding responsive

--- src/component/App/AboutMe/index.js
+++ src/component/App/AboutMe/index.js
@@ -1,5 +1,6 @@
 import './styles.scss';
 import photo from './photo.jpeg';
+import laptopIcon from './laptop-1928.svg'
 
 function AboutMe() {
   return (
@@ -9,35 +10,42 @@ function AboutMe() {
         <p className='aboutMe-text'>I am a passionate web developer who has completed training 
         in web and mobile development and is looking for a 16-month alternating position 
         (1 week at school, 2 weeks in a company) followed by 3 months of full-time 
-        work in a company. My goal is to join an innovative and dynamic company to continue 
+        work in a company. <br/> My goal is to join an innovative and dynamic company to continue 
         learning, develop my technical skills, and work on exciting projects.</p>
 
         <h2 className='aboutMe-keywords'>skills</h2>
-        <p className='aboutMe-text'>Programming languages: HTML, CSS, JavaScript, PHP</p>
-        <p className='aboutMe-text'>Design and development of websites and mobile applications</p>
-        <p className='aboutMe-text'>Knowledge of frameworks: React, Bootstrap, Laravel, Symfony</p>
-        <p className='aboutMe-text'>Management of MySQL databases, Adminer</p>
-        <p className='aboutMe-text'>Ability to work in a team, solve problems and learn quickly.</p>
+          <p className='aboutMe-text'>Programming languages: HTML, CSS, JavaScript, PHP</p>
+          <p className='aboutMe-text'>Design and development of websites and mobile applications</p>
+          <p className='aboutMe-text'>Knowledge of frameworks: React, Bootstrap, Laravel, Symfony</p>
+          <p className='aboutMe-text'>Management of MySQL databases, Adminer</p>
+          <p className='aboutMe-text'>Ability to work in a team, solve problems and learn quickly.</p>
+
+          <h2 className='aboutMe-keywords'>Diploma</h2>
+          <p className='aboutMe-text'>BAC STMG -{'>'} Highest qualification obtained at a French upper secondary school (lycée), with a focus on business management</p>
+          <p className='aboutMe-text'>CQP APS and SSIAP1 -{'>'} Fire Protection Services: The protection of life and safety of persons and properties from fire, including everything relating to public </p>
+          <p className='aboutMe-text'>DWWM (BAC +2) -{'>'} Developer Web and Web Mobile</p>
+
+        <h2 className='aboutMe-keywords'>Projects</h2>
+          <p className='aboutMe-text'>You can see my individual projects *here*</p>
 
       </div>
-          <div className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                  <p className="title">Identity Card</p>
-                  <p>Hover Me</p>
-                </div>
-                <div className="flip-card-back">
-                  <img className="photo" src={photo}/>
-                  <p className="title">Developper Web front-end</p>
-                  <p className='adress'>From Bonnières-sur-seine</p>
-                  <p className='age'>27 ans</p>
-                </div>
-
-                <div className='aboutMe-content'>
-            
-                </div>
-              </div>
+      <div className="flip-card-container">
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img className="laptopIcon" src={laptopIcon}/>
+              <p className="title">Identity Card</p>
+              <p>Hover Me</p>
+            </div>
+            <div className="flip-card-back">
+                <img className="photo" src={photo}/>
+                <p className="title">Developper Web front-end</p>
+                <p className='adress'>27 years old</p>
+                <p className='age'>From Bonnières-sur-seine</p>
+            </div>
           </div>
+        </div>
+      </div>
     </div>
   );
 }

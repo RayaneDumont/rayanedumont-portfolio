c4c8b00eee56deb10efb9ee1f69c002406bb30a3
RayaneDumont <rayane.dumont@gmail.com>
2023-05-08 08:57:00 +0200

    added style to playbutton + adding social medias icons to the AboutMe pag

--- src/component/App/Music/index.js
+++ src/component/App/Music/index.js
@@ -33,12 +33,16 @@ function Music() {
   return (
     <div className='player-container'>
       <audio ref={audioRef} src={selectedAudio} />
-
       <div className='player-buttons'>
         {isPlaying ? (
           <button onClick={handlePause}>Pause</button>
         ) : (
-          <button onClick={handlePlay}>Play</button>
+          <button onClick={handlePlay} className='play'>
+            <svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="26px">
+              <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" fill="currentColor">
+                </path>
+            </svg>
+          </button>
         )}
         <button onClick={handleStop}>Stop</button>
       </div>
